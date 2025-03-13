618f7032092abd87df240a61b086f590a522108f
Giratina99MM <ryangomesramos2018@gmail.com>
2025-03-13 19:22:50 -0300

    ex-03.py

---
+++ ex-03.py
@@ -0,0 +1,24 @@
+3. Letra de música - Faça um programa que imprima na tela o nome de uma música que você gosta (proibido letras do Justin Bieber)
+print("[Taka filhote]")
+print("Quando eu for rei")
+print("Nenhum dos bichos vai me enfrentar, me enfrentar")
+print("Irmãozinho")
+
+print("[Mufasa filhote]")
+print("Maninho")
+
+print("[Taka filhote]")
+print("Ao meu lado, você estará")
+
+print("[Mufasa filhote]")
+print("Ha! Sei lá")
+
+print("[Taka filhote]")
+print("Vem cá")
+print("Quando eu for rei")
+
+print("[Mufasa filhote]")
+print("Quando for rei")
+
+print("[Taka filhote]")
+print("Nenhum dos bichos vai me provocar, provocar")
