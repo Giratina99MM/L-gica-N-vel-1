5b837596f861469528a68dd78ae82a31addb1bf1
Giratina99MM <ryangomesramos2018@gmail.com>
2025-03-13 19:47:39 -0300

    ex-07.py

---
+++ ex-07.py
@@ -0,0 +1,13 @@
+#7. Pinheiro  - Implemente um programa que desenhe um "pinheiro" na tela, similar ao abaixo.
+#Enriqueça o desenho com outros caracteres, simulando enfeites.
+print("        ✫")
+print("       Xxx")
+print("      xxxxx")
+print("     X7XXXXX")
+print("    XXXXXX8XX")
+print("   XXRXXXXXOXX")
+print("  XOXXXXXXXMXXX")
+print(" XXXXXXHXXXXXXOX")
+print("        XX")
+print("        XX")
+print("       XXXX")
