d29e5a626b9ea6aae1b91650835b0e9d64e0e9a2
Giratina99MM <ryangomesramos2018@gmail.com>
2025-03-20 19:24:30 -0300

    ex-12.py

---
+++ ex-12.py
@@ -0,0 +1,6 @@
+#Escreva um algoritmo para ler as dimensões de um retângulo (base e altura), calcular e escrever a área do retângulo.
+print("Qual a base?")
+base = int(input())
+print("Qual a altura?")
+altura = int(input())
+print(base * altura)
