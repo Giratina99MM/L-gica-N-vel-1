bcfd4914998264dec4f3283a40b3e5d44ad286f9
Giratina99MM <ryangomesramos2018@gmail.com>
2025-03-20 18:48:33 -0300

    ex-09.py

---
+++ ex-09.py
@@ -0,0 +1,7 @@
+#9 - escreva um software que some dois numeros.
+print("Qual o primeiro número?")
+numero1 = int(input())
+print("Qual o segundo número?")
+numero2 = int(input())
+
+print(numero1 + numero2)
