d47f1fe84975416c0223b3886b26cf4b66328088
Giratina99MM <ryangomesramos2018@gmail.com>
2025-03-20 19:17:27 -0300

    ex-10.py

---
+++ ex-10.py
@@ -0,0 +1,21 @@
+#10 - problema: escreva um software informe a média aritimética de um aluno que cursou 4 bimestres
+#Etapas para programar
+
+#1 - obter as 4 notas do aluno
+#2 - efetuar a conta
+#3 - mostrar a média aritimética
+
+print("Nota 1")
+
+nota1 = int(input())
+print("Nota 2")
+
+nota2 = int(input())
+print("Nota 3")
+
+nota3 = int(input())
+print("Nota 4")
+
+nota4 = int(input())
+soma = (nota1 + nota2 + nota3 + nota4)
+print(soma/4)
